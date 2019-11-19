931b0b026b99cc2be87134300efee09bd5c723f6
Michael House <mjhouse@protonmail.com>
2019-11-19 08:43:44 -0600

    Adds basic diesel support

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 
 #[macro_use] extern crate log;
 #[macro_use] extern crate rocket;
-#[macro_use] extern crate rusqlite;
 extern crate simplelog;
 extern crate medallion;
 extern crate base64;
@@ -24,85 +23,85 @@ use rocket::{
     response::{Redirect}
 };
 
-use simplelog::*;
+// use simplelog::*;
 
-mod user;
-mod post;
-mod session;
+// mod user;
+// mod post;
+// mod session;
 
-use user::*;
-use post::*;
-use session::*;
+// use user::*;
+// use post::*;
+// use session::*;
 
-const STATIC_CSS:  &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/css");
-const STATIC_JS:   &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/js");
-const STATIC_IMG:  &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/img");
-const STATIC_FONT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/font");
+// const STATIC_CSS:  &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/css");
+// const STATIC_JS:   &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/js");
+// const STATIC_IMG:  &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/img");
+// const STATIC_FONT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static/font");
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Site {
-    name: String,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Site {
+//     name: String,
+// }
 
-#[get("/")]
-fn index(site: State<Site>) -> Template {
-    Template::render("index", &(*site))
-}
+// #[get("/")]
+// fn index(site: State<Site>) -> Template {
+//     Template::render("index", &(*site))
+// }
 
-#[get("/login")]
-fn login_get(_session: Session, site: State<Site>) -> Template {
-    Template::render("login", &(*site))
-}
+// #[get("/login")]
+// fn login_get(_session: Session, site: State<Site>) -> Template {
+//     Template::render("login", &(*site))
+// }
 
-#[get("/profile")]
-fn profile(session: Session) -> Result<Template,Redirect> {
-    if session.user.is_some(){
-        Ok(Template::render("profile", &session))
-    }
-    else {
-        Err(Redirect::to("/login"))
-    }
-}
+// #[get("/profile")]
+// fn profile(session: Session) -> Result<Template,Redirect> {
+//     if session.user.is_some(){
+//         Ok(Template::render("profile", &session))
+//     }
+//     else {
+//         Err(Redirect::to("/login"))
+//     }
+// }
 
-#[post("/login", data = "<login_form>")]
-fn login_post(session: Session, mut cookies: Cookies, login_form: Form<LoginForm>) -> Redirect {
-    if session.login(&mut cookies,&login_form).is_some() {
-        Redirect::to("/profile")
-    }
-    else {
-        Redirect::to("/login")
-    }
-}
+// #[post("/login", data = "<login_form>")]
+// fn login_post(session: Session, mut cookies: Cookies, login_form: Form<LoginForm>) -> Redirect {
+//     if session.login(&mut cookies,&login_form).is_some() {
+//         Redirect::to("/profile")
+//     }
+//     else {
+//         Redirect::to("/login")
+//     }
+// }
 
-#[post("/register", data = "<register_form>")]
-fn register_post(session: Session, register_form: Form<RegisterForm>) -> Redirect {
-    let result = session.register(&register_form);
-    dbg!(result);
+// #[post("/register", data = "<register_form>")]
+// fn register_post(session: Session, register_form: Form<RegisterForm>) -> Redirect {
+//     let result = session.register(&register_form);
+//     dbg!(result);
 
-    Redirect::to("/login")
-}
+//     Redirect::to("/login")
+// }
 
-#[get("/logout")]
-fn logout(session: Session, mut cookies: Cookies) -> Redirect {
-    session.logout(&mut cookies);
-    Redirect::to("/")
-}
+// #[get("/logout")]
+// fn logout(session: Session, mut cookies: Cookies) -> Redirect {
+//     session.logout(&mut cookies);
+//     Redirect::to("/")
+// }
 
 fn main() {
-    let site = Site {
-        name: String::from("Michael House")
-    };
+    // let site = Site {
+    //     name: String::from("Michael House")
+    // };
 
-    rocket::ignite()
-        .attach(Template::fairing())
-        .manage(site)
-        .mount("/", routes![index,
-            login_get, login_post, register_post,
-            logout, profile
-        ])
-        .mount("/css",  StaticFiles::from(STATIC_CSS ))
-        .mount("/js",   StaticFiles::from(STATIC_JS  ))
-        .mount("/img",  StaticFiles::from(STATIC_IMG ))
-        .mount("/font", StaticFiles::from(STATIC_FONT))
-        .launch();
+    // rocket::ignite()
+    //     .attach(Template::fairing())
+    //     .manage(site)
+    //     .mount("/", routes![index,
+    //         login_get, login_post, register_post,
+    //         logout, profile
+    //     ])
+    //     .mount("/css",  StaticFiles::from(STATIC_CSS ))
+    //     .mount("/js",   StaticFiles::from(STATIC_JS  ))
+    //     .mount("/img",  StaticFiles::from(STATIC_IMG ))
+    //     .mount("/font", StaticFiles::from(STATIC_FONT))
+    //     .launch();
 }
--- src/post.rs
+++ src/post.rs
@@ -1,24 +1,5 @@
 use chrono::{DateTime, TimeZone, NaiveDateTime, Utc};
 
-macro_rules! impl_from {
-    ( $n:ident, $q:ident, $t:ty ) => {
-        pub fn $n( t_value:$t ) -> Result<User,DatabaseError> {
-            Connection::open(DATABASE)
-            .or_else(logs!(DatabaseError::NoConnection))
-            .and_then(|c|{
-                c.query_row($q,params![t_value],
-                    |row| Ok(Pose {
-                            id:       row.get(0)?,
-                            title:    row.get(1)?,
-                            body:     row.get(2)?,
-                            author:   row.get(3)?,
-                            created:  DateTime::parse_from_rfc3339(row.get(4)?),
-                        })
-                ).or_else(logs!(DatabaseError::QueryFailed))
-            })
-        }
-    };
-}
 
 struct Post {
     id:      i64,
@@ -30,6 +11,5 @@ struct Post {
 
 impl Post {
 
-    impl_from!(from_rowid,SELECT_ROWID,i64);
 
 }
